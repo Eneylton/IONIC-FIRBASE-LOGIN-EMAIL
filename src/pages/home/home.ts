05c6afb572d66c7fe9e8d26357349697bd77fd37
eneylton <eneylton@hotmail.com>
2019-06-23 12:39:02 -0300

    Validando o Email com FireBase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AuthService } from '../../providers/auth/auth-service';
 
+
+@IonicPage({})
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private authService: AuthService) {
   }
 
+  signOut(){
+    this.authService.signOut()
+    .then(() => {
+
+      this.navCtrl.setRoot('SignupPage');
+    }).catch((error)=>{
+      console.log(error)  
+    });
+  }
 }
